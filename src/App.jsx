50fc7a2a3fec4e233f80a706c2f85d5240cec509
leesangha <sanghalee1@naver.com>
2024-09-06 11:30:33 +0900

    feat: kbo 랭킹차트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import "./App.css";
+import { KBORanks } from "./statics/kbo-rank";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <table className="rank-table">
+        <thead>
+          <tr>
+            <td>순위</td>
+            <td>팀명</td>
+            <td>경기</td>
+            <td>승</td>
+            <td>패</td>
+            <td>무</td>
+            <td>승률</td>
+            <td>게임차</td>
+          </tr>
+        </thead>
+        <tbody>
+          {KBORanks.map((team, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{team.name}</td>
+                <td>{team.games}</td>
+                <td>{team.win}</td>
+                <td>{team.lose}</td>
+                <td>{team.draw}</td>
+                <td>{team.winningPercentage}</td>
+                <td>{team.gameDiff}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
